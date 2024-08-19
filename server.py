3dae8661b1d52ddee251b74bb9c8fcbcaf4c4637
niki <nikolett0203@gmail.com>
2024-08-19 00:31:42 -0400

    help

--- server.py
+++ server.py
@@ -5,11 +5,10 @@ from flask import Flask, render_template
 # creates an instance of the class flask, calls it app
 app = Flask(__name__)
 
-
 @app.route('/')
 # create function that executes when user visits the url
 def home_page():
     return render_template('index.html')
 
-# if __name__ == '__main__':
-#     app.run(debug=False)
+if __name__ == '__main__':
+    app.run(debug=False)
